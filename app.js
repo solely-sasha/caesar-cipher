87aba85ad00d744c5751179b739458ba4115d32b
Sasha Sims <solely.sashasims@gmail.com>
2023-04-13 16:12:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var readline = require("readline-sync");
+var input = readline
+  .question("What phrase would you like to encrypt? ")
+  .toLowerCase();
+var shift = parseInt(
+  readline.question("How many letters would you like to shift? ")
+);
